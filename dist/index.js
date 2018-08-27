a37218c467535075b73105d1c560bce693a184b3
Adrian Helvik <adrian@adrian.local>
2018-08-27 15:30:23 +0200

    ...

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _lex = require('./lex');
+
+var _lex2 = _interopRequireDefault(_lex);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _lex2.default;
